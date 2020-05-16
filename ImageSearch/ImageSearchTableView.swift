a13001b8568e2c231ca4f0137985393ae7f25dfb
Shilpa Garg <shilpag@dewsolutions.in>
2020-05-17 00:32:57 +0530

    base

---
+++ ImageSearch/ImageSearchTableView.swift
@@ -0,0 +1,14 @@
+//
+//  ImageSearchTableView.swift
+//  ImageSearch
+//
+//  Created by Shilpa Garg on 17/05/20.
+//  Copyright © 2020 Shilpa Garg. All rights reserved.
+//
+
+import UIKit
+
+class ImageSearchTableView: UIView {
+
+
+}
